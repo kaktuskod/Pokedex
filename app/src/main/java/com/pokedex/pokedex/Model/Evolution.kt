e00d91ff88766bf43a4c489240dc053df9eb8949
kaktuskod <fatihi.yassine@yahoo.fr>
2020-09-17 15:15:35 +0200

    Creation app pokedex Kotlin List Pokemon step

---
+++ app/src/main/java/com/pokedex/pokedex/Model/Evolution.kt
@@ -0,0 +1,7 @@
+package com.pokedex.pokedex.Model
+
+class Evolution {
+    var num:String?=null
+    var name:String?=null
+
+}
---
+++ app/src/main/java/com/pokedex/pokedex/Model/Pokedex.kt
@@ -0,0 +1,6 @@
+class Pokedex {
+    /*
+    public List<Pokemon> pokemon { get; set; }
+     */
+    var pokemon:List<Pokemon>? = null
+}
