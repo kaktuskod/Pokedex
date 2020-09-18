f94480e0b9334db1ea98c1e8f6895bd2c5683921
kaktuskod <fatihi.yassine@yahoo.fr>
2020-09-18 13:25:07 +0200

    change pokemon list item

--- app/src/main/java/com/pokedex/pokedex/PokemonList.kt
+++ app/src/main/java/com/pokedex/pokedex/PokemonList.kt
@@ -3,7 +3,8 @@ package com.pokedex.pokedex
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
-
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.pokedex.pokedex.Adapter.PokemonListAdapter
