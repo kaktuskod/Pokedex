92124becd270a7500c234be4ead40bf527ae66a9
kaktuskod <fatihi.yassine@yahoo.fr>
2020-09-18 04:00:46 +0200

    Add INTERNET permision in manifest

---
+++ app/src/main/java/com/pokedex/pokedex/Common/Common.kt
@@ -0,0 +1,8 @@
+package com.pokedex.pokedex.Common
+
+import Pokemon
+import com.pokedex.pokedex.PokemonList
+
+object Common {
+    val pokemonList:List<Pokemon> = ArrayList()
+}
--- app/src/main/java/com/pokedex/pokedex/PokemonList.kt
+++ app/src/main/java/com/pokedex/pokedex/PokemonList.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.pokedex.pokedex.Adapter.PokemonListAdapter
+import com.pokedex.pokedex.Common.Common
+import com.pokedex.pokedex.Common.ItemOffsetDecoration
 import com.pokedex.pokedex.Retrofit.IPokemonList
 import com.pokedex.pokedex.Retrofit.RetrofitClient
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_pokemon_list.*
 
 class PokemonList : Fragment() {
 
@@ -16,15 +23,33 @@ class PokemonList : Fragment() {
 
     init {
         var retrofit = RetrofitClient.instance
-        iPokemonList = retrofit.Create()
+        iPokemonList = retrofit.create(IPokemonList::class.java)
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokemon_list, container, false)
+        val itemView = inflater.inflate(R.layout.fragment_pokemon_list, container, false)
+        pokemon_recyclerview.setHasFixedSize(true)
+        pokemon_recyclerview.layoutManager = GridLayoutManager(activity, 2)
+        val itemDecoration = ItemOffsetDecoration(activity!!, R.dimen.spacing)
+        pokemon_recyclerview.addItemDecoration(itemDecoration)
+        fetchData()
+
+        return itemView
+    }
+
+    private fun fetchData() {
+        compositeDisposable.add(iPokemonList.listPokemon
+            .subscribeOn(Schedulers.io()))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{ pokemon ->
+                Common.pokemonList = pokemonDex.pokemon!!
+                val adapter = PokemonListAdapter(activity!!, Common.pokemonList)
+                pokemon_recyclerview.adapter = adapter
+            }
     }
 
 
-    }
+}
